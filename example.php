f5c77cc2cb124bf8ecb76b3265732fc6a29e7e1f
justineAm <jepoyambrad8@gmail.com>
2021-05-13 13:13:26 +0800

    example php is added

---
+++ example.php
@@ -0,0 +1,61 @@
+
+<?php
+require_once 'navs.php'
+?>
+
+
+
+<div class="container-fluid">
+
+
+
+    <div class="row">
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                    <div>
+                        <img src="https://images.ecency.com/p/o1AJ9qDyyJNSpZWhUgGYc3MngFqoAN69qSYg17rrvgpyfPJJS?format=match&mode=fit" alt="..." class="img-thumbnail">
+                    </div>
+                        <div class="col mr-2 mt-3">
+                            <div class="text-lg font-weight-bold text-primary text-center text-uppercase mb-1">
+                                Pritong Baki</div>
+                            <div class="mb-0 font-weight-bold text-md text-center text-gray-800">PHP 40,000
+                            </div>
+                        </div>
+                </div>
+            </div>
+        </div>
+
+        
+    </div>
+    <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                    <div class="row no-gutters align-items-center">
+                    <div>
+                    <div class="form-group text-center text-lg mt-5">
+      <label for="total">Total:</label>
+      <input type="text" class="form-control" id="total" value="" disabled >
+    </div>
+                    </div>
+                        <div class="col mr-2 mt-3 text-center">
+                        <button type="button" class="btn btn-primary">Settle Payment</button>
+
+                        </div>
+                </div>
+            </div>
+        </div>
+</div>
+
+    
+
+        
+
+        
+        
+</div>
+</body>
+
+</html>
