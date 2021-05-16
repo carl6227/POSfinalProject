355315f1921a2ddd71a09895d08fbf13d6ae26b7
2ndyrGroupA <2ndyrGroupA@pn.local>
2021-05-16 21:50:04 +0800

    card resized

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ require_once 'navs.php'
         $statement->execute();
         $saleDate = $statement->fetch();
         echo '
-                <div class="col-xl-5 col-md-6 mb-4">
+                <div class="col-xl-g col-md-6 mb-4">
                     <div class="card border-left-primary shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
