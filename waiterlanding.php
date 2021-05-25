54462a5bcb71fbbe741072e7e1b75262014c5f53
cjcabungcag <chillajean.cabungcag@student.passerellesnumeriques.org>
2021-05-25 16:07:31 +0800

    preloader is done

--- waiterlanding.php
+++ waiterlanding.php
@@ -21,125 +21,134 @@ session_start();
     
 
 ?>
-
+<div class="load">
+    <hr />
+    <hr />
+    <hr />
+    <hr />
+</div>
 <!-- Begin Page Content -->
-<div class="container-fluid">
+<div class="preload" style="display:none">
+    <div class="container-fluid">
 
-    <div class="container">
-        <h2 class="tableIndicator">Menu</h2>
-        <div class="row">
-            <div class="container-fluid orderWrapper" style="overflow-y:auto;height:1000px; margin-bottom:-350px">
+        <div class="container">
+            <h2 class="tableIndicator">Menu</h2>
             <div class="row">
-                <?php $myRestruant->dispMenuForWaiter();?>
-            </div>
-               
-            </div>
-            
-
-            <div class="container-fluid tableWrapper" style="display:none">
-
-                <button class="btn btn-info float-right mb-4 addBtn" data-toggle="modal"
-                    data-target="#addOrderModal"><i class="fa fa-plus"></i> Add Order</button>
-            </div>
-            <div class="container-fluid mb-5" style="overflow-y:auto;height: 200px;">
-                <table class="table table-hover tableWrapper" >
-                    <thead >
-                        <tr>
-                            <th>Menu Name</th>
-                            <th>price</th>
-                            <th>quantity</th>
-                            <th>subtotal</th>
-                            <th>status</th>
-                            <th>action</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-
-                    </tbody>
-                </table>
-
-            </div>
-            <div>
-            <hr class="bg-dark">
-            </div>
-            
-         <!-- totalbill container -->
-        <div class="container billWrapper mb-5 ">
-       
-    </div>
-   
-        <!-- Add order Modal -->
-        <div class="modal fade" id="addOrderModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-            aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Add Order</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
+                <div class="container-fluid orderWrapper" style="overflow-y:auto;height:1000px; margin-bottom:-350px">
+                    <div class="row">
+                        <?php $myRestruant->dispMenuForWaiter();?>
                     </div>
-                    <form action="#" method="post">
-                        <div class="modal-body">
-                            <div class="input-group input-group-lg mb-4">
-                                <input type="text" id="categoriesDropDown" name="category" class="form-control"
-                                    aria-label="Text input with dropdown button">
-                                <div class="input-group-append ">
-                                    <button class="btn btn-outline-secondary dropdown-toggle" type="button"
-                                        data-toggle="dropdown" aria-haspopup="true"
-                                        aria-expanded="false">Categories</button>
-                                    <div class="dropdown-menu">
-                                        <?php echo  $myRestruant->getCategories()?>
+
+                </div>
+
+
+                <div class="container-fluid tableWrapper" style="display:none">
+
+                    <button class="btn btn-info float-right mb-4 addBtn" data-toggle="modal"
+                        data-target="#addOrderModal"><i class="fa fa-plus"></i> Add Order</button>
+                </div>
+                <div class="container-fluid mb-5" style="overflow-y:auto;height: 200px;">
+                    <table class="table table-hover tableWrapper">
+                        <thead>
+                            <tr>
+                                <th>Menu Name</th>
+                                <th>price</th>
+                                <th>quantity</th>
+                                <th>subtotal</th>
+                                <th>status</th>
+                                <th>action</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+
+                        </tbody>
+                    </table>
+
+                </div>
+                <div>
+                    <hr class="bg-dark">
+                </div>
+
+                <!-- totalbill container -->
+                <div class="container billWrapper mb-5 ">
+
+                </div>
+
+                <!-- Add order Modal -->
+                <div class="modal fade" id="addOrderModal" tabindex="-1" role="dialog"
+                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Add Order</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <form action="#" method="post">
+                                <div class="modal-body">
+                                    <div class="input-group input-group-lg mb-4">
+                                        <input type="text" id="categoriesDropDown" name="category" class="form-control"
+                                            aria-label="Text input with dropdown button">
+                                        <div class="input-group-append ">
+                                            <button class="btn btn-outline-secondary dropdown-toggle" type="button"
+                                                data-toggle="dropdown" aria-haspopup="true"
+                                                aria-expanded="false">Categories</button>
+                                            <div class="dropdown-menu">
+                                                <?php echo  $myRestruant->getCategories()?>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                            <div class="input-group input-group-lg mb-4">
-                                <input type="text" class="form-control" name="menuName"
-                                    aria-label="Text input with dropdown button">
-                                <div class="input-group-append ">
-                                    <button class="btn btn-outline-secondary dropdown-toggle" type="button"
-                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Items</button>
-                                    <div class="dropdown-menu itemData">
+                                    <div class="input-group input-group-lg mb-4">
+                                        <input type="text" class="form-control" name="menuName"
+                                            aria-label="Text input with dropdown button">
+                                        <div class="input-group-append ">
+                                            <button class="btn btn-outline-secondary dropdown-toggle" type="button"
+                                                data-toggle="dropdown" aria-haspopup="true"
+                                                aria-expanded="false">Items</button>
+                                            <div class="dropdown-menu itemData">
 
+                                            </div>
+                                        </div>
                                     </div>
+
+                                    <div class="col-sm-6">
+                                        <div class="input-group input-group-lg">
+                                            <input type="number" min="1" value="1" class="form-control" name="quantity"
+                                                placeholder="quantity">
+
+                                        </div>
+                                    </div>
+
+
+                                    <input type="hidden" min="1" max="5" class="form-control tableNo" name="tablenum"
+                                        placeholder="Table No.">
+
+
+
                                 </div>
-                            </div>
-
-                            <div class="col-sm-6">
-                                <div class="input-group input-group-lg">
-                                    <input type="number" min="1" value="1"class="form-control" name="quantity"
-                                        placeholder="quantity">
-
-                                </div>
-                            </div>
-
-
-                            <input type="hidden" min="1" max="5" class="form-control tableNo" name="tablenum"
-                                placeholder="Table No.">
-
-
-
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary btn-lg"
+                                        data-dismiss="modal">Close</button>
+                                    <button type="submit" name="addOrder" class="btn btn-primary btn-lg">Add
+                                        Order</button>
+                            </form>
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary btn-lg" data-dismiss="modal">Close</button>
-                            <button type="submit" name="addOrder" class="btn btn-primary btn-lg">Add Order</button>
-                    </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
 
-           
-    <!-- /.container-fluid 
+
+            <!-- /.container-fluid 
         -->
 
-</div>
+        </div>
 
-<!-- End of Main Content -->
+        <!-- End of Main Content -->
 
-</div>
-<!-- End of Content Wrapper -->
+    </div>
+    <!-- End of Content Wrapper -->
 
 </div>
 <!-- End of Page Wrapper -->
@@ -184,8 +193,8 @@ session_start();
                 </button>
             </div>
             <div class="modal-body">
-            <form method="post">
-            <?php
+                <form method="post">
+                    <?php
                 
    
                 $user = $_SESSION['username'];
@@ -214,25 +223,30 @@ session_start();
                 </div>
                         ';
             ?>
-          
-            
+
+
             </div>
             <div class="modal-footer">
-                
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <button class="btn btn-primary" type="submit" name="editProfileBtn">Update</button>
+
+                <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                <button class="btn btn-primary" type="submit" name="editProfileBtn">Update</button>
                 </form>
 
             </div>
         </div>
     </div>
 </div>
-
+    <script>
+        $(document).ready(function() {
+            setTimeout(function() {
+                $('.load').remove()
+                $('.preload').show()
+            }, 1500);
+        });
+    </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script type="text/javascript" src="ajax_request.js">
-
-
 </script>
 
 </body>
